3cdd83428cc8de49ce72d149aab219f93012138c
dungvip <DUNG@DESKTOP-92GIOR2>
2022-11-30 13:47:24 +0700

    pin mau sac ram

---
+++ 1.DAL/IRepositories/IMauSacRepository.cs
@@ -0,0 +1,12 @@
+﻿using _1.DAL.DomainModels;
+
+namespace _1.DAL.IRepositories
+{
+    public interface IMauSacRepository
+    {
+        bool Add(MauSac obj);
+        bool Update(MauSac obj);
+        bool Delete(MauSac obj);
+        List<MauSac> GetAll();
+    }
+}
---
+++ 1.DAL/IRepositories/IPinRepository.cs
@@ -0,0 +1,12 @@
+﻿using _1.DAL.DomainModels;
+
+namespace _1.DAL.IRepositories
+{
+    public interface IPinRepository
+    {
+        bool Add(Pin obj);
+        bool Update(Pin obj);
+        bool Delete(Pin obj);
+        List<Pin> GetAll();
+    }
+}

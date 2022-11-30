18370b7d61414c82124bb3739d1d1d3637d667dd
danhtung <danhtung1601@gmail.com>
2022-11-30 13:39:51 +0700

    code bonhochipmanhinh

---
+++ 1.DAL/IRepositories/IChipRepository.cs
@@ -0,0 +1,12 @@
+﻿using _1.DAL.DomainModels;
+
+namespace _1.DAL.IRepositories
+{
+    public interface IChipRepository
+    {
+        bool Add(Chip obj);
+        bool Update(Chip obj);
+        bool Delete(Chip obj);
+        List<Chip> GetAll();
+    }
+}
---
+++ 1.DAL/IRepositories/IManHinhRepository.cs
@@ -0,0 +1,12 @@
+﻿using _1.DAL.DomainModels;
+
+namespace _1.DAL.IRepositories
+{
+    public interface IManHinhRepository
+    {
+        bool Add(ManHinh obj);
+        bool Update(ManHinh obj);
+        bool Delete(ManHinh obj);
+        List<ManHinh> GetAll();
+    }
+}

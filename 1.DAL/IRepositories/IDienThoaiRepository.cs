933cce2681cc1a6b021071f30a752577285dc1a4
le hieu <lefhym237@gmail.com>
2022-11-30 13:44:26 +0700

    Nsx, DT

---
+++ 1.DAL/IRepositories/IDienThoaiRepository.cs
@@ -0,0 +1,12 @@
+﻿using _1.DAL.DomainModels;
+
+namespace _1.DAL.IRepositories
+{
+    public interface IDienThoaiRepository
+    {
+        bool Add(DienThoai obj);
+        bool Update(DienThoai obj);
+        bool Delete(DienThoai obj);
+        List<DienThoai> GetAll();
+    }
+}
---
+++ 1.DAL/Repositories/NsxRepository.cs
@@ -0,0 +1,58 @@
+﻿using _1.DAL.Context;
+using _1.DAL.DomainModels;
+using _1.DAL.IRepositories;
+
+namespace _1.DAL.Repositories
+{
+    public class NsxRepository : INsxRepository
+    {
+        private CuaHangDbContext dbContext;
+
+        public NsxRepository()
+        {
+            dbContext = new CuaHangDbContext();
+        }
+
+        public bool Add(Nsx obj)
+        {
+            if (obj != null)
+            {
+                dbContext.Nsxes.Add(obj);
+                dbContext.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public bool Update(Nsx obj)
+        {
+            if (obj != null)
+            {
+                var temp = dbContext.Nsxes.FirstOrDefault(c => c.Id == obj.Id);
+                temp.Ten = obj.Ten;
+                temp.TrangThai = obj.TrangThai;
+                dbContext.Update(temp);
+                dbContext.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public bool Delete(Nsx obj)
+        {
+            if (obj != null)
+            {
+                var temp = dbContext.Nsxes.FirstOrDefault(c => c.Id == obj.Id);
+                dbContext.Remove(temp);
+                dbContext.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public List<Nsx> GetAll()
+        {
+            return dbContext.Nsxes.ToList();
+        }
+    }
+}

e882850d50303f8aecaa097574e112d4892525a2
le hieu <lefhym237@gmail.com>
2022-12-05 21:10:22 +0700

    fixed quên mật khẩu boss

--- 3.PL/Program.cs
+++ 3.PL/Program.cs
@@ -16,7 +16,7 @@ namespace _3.PL
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new FrmDashBoard());
+            Application.Run(new FrmDangNhap());
         }
     }
 }
